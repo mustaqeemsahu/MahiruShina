bde03f4cf8e87e6009be8ac80b9a27ab0f322845
Mohammed Mustaqeem <mustaqeemsahu@gmail.com>
2026-05-09 22:39:57 +0530

    Written Change

--- utils/filters.py
+++ utils/filters.py
@@ -41,7 +41,7 @@ async def force_sub(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [[InlineKeyboardButton("📢 Join Channel", url=f"https://t.me/{FORCE_CHANNEL.replace('@','')}")]]
 
     await update.message.reply_text(
-        "❌ <b>Access Denied!</b>\n\nYou must join our official channel to use this bot and search anime.",
+        "❌ 𝗔𝗖𝗖𝗘𝗦 𝗗𝗘𝗡𝗜𝗘𝗗!!!\n\n𝐘𝐎𝐔 𝐌𝐔𝐒𝐓 𝐉𝐎𝐈𝐍 𝐎𝐔𝐑 𝐌𝐀𝐈𝐍 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝐓𝐎 𝐔𝐒𝐄 𝐓𝐇𝐈𝐒 𝐁𝐎𝐓 𝐀𝐍𝐃 𝐒𝐄𝐀𝐑𝐂𝐇 𝐔𝐍𝐋𝐈𝐌𝐈𝐓𝐄𝐃 𝐀𝐍𝐈𝐌𝐄 𝐇𝐄𝐑𝐄.\n\n𝐓𝐇𝐀𝐍𝐊 𝐘𝐎𝐔 & 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 𝐔𝐒",
         reply_markup=InlineKeyboardMarkup(keyboard)
     )
 
