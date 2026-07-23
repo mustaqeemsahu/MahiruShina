99853ab735a41e0bcdc1dcc5fabb80a306d1600b
Mohammed Mustaqeem <mustaqeemsahu@gmail.com>
2026-07-23 18:27:20 +0530

    Update Popup mesaage

--- handlers/callback.py
+++ handlers/callback.py
@@ -106,6 +106,12 @@ async def button_click(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
                 return
 
+elif data == "ignore":
+    await query.answer(
+        "Please Use Prev/Next Butn Only",
+        show_alert=False
+    )
+
 # ==============================
 # PAGINATION
 # ==============================
